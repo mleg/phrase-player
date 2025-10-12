3d8d4cc587263c64fed5c3d720ce4798687a60c8
Маслов Олег <ovmaslo9@mts.ru>
2025-10-12 20:55:28 +0300

    Fix Vite config for Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "https://mleg.github.io/phrase-player/"
+      : "/",
   plugins: [
     react({
       babel: {
